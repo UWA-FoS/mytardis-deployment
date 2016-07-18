4c2647a2ecd7d2512c6449805c1e1edc3aaacb1c
Dean Taylor <dean.taylor@uwa.edu.au>
2016-07-18 09:57:20 +0800

    Added settings.py with Docker environment variables for configuration. fixed order of manage.py instructions to create tables on first run before migrations.

--- mytardis/app/docker-entrypoint.sh
+++ mytardis/app/docker-entrypoint.sh
@@ -13,10 +13,11 @@ if ! grep -q '^SECRET_KEY=' tardis/settings.py; then
 python -c "import os; from random import choice; key_line = '%sSECRET_KEY=\"%s\"  # generated from build.sh\n' % ('from tardis.settings_changeme import * \n\n' if not os.path.isfile('tardis/settings.py') else '', ''.join([choice('abcdefghijklmnopqrstuvwxyz0123456789\\!@#$%^&*(-_=+)') for i in range(50)])); f=open('tardis/settings.py', 'a+'); f.write(key_line); f.close()"
 fi
 
-python manage.py migrate	# Apply database migrations
-python manage.py collectstatic --noinput	# Collect static files
 python manage.py createcachetable default_cache
 python manage.py createcachetable celery_lock_cache
+python manage.py collectstatic --noinput	# Collect static files
+
+python manage.py migrate	# Apply database migrations
 
 # Prepare log files and start outputting logs to stdout
 touch /srv/logs/gunicorn.log
